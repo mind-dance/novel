f9bec3e490f6dfa960e32a85f5dc86ab350a36c1
Vincent <suppernoname@163.com>
2024-05-02 02:36:43 +0800

    format chap1

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,14 +12,19 @@ export default defineConfig({
       { text: '少年阿宾', link: '/少年阿宾/第1章-房东太太'}
     ],
 
-    sidebar: [
-      {
-        text: '少年阿宾',
-        items: [
-          { text: '第一章', link: '/少年阿宾/第1章-房东太太'}
-        ]
-      }
-    ]
+    sidebar: {
+      '/少年阿宾/': [
+        {
+          text: '少年阿宾',
+          items: [
+            { text: '目录', link:'/少年阿宾/toc'},
+            { text: '第一章 房东太太', link: '/少年阿宾/第1章-房东太太'}
+          ]
+        }
+      ]
+    }
+    
+    
 
     // socialLinks: [
     //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
--- index.md
+++ index.md
@@ -16,6 +16,8 @@ features:
     details: 在原作的基础上对错别字等稍作修改，并进行简中本土化。
   - title: 共享
     details: 原著内容归原作者所有，修改的内容以CC-0协议进行分享。
+  - title: 去中心化
+    details: 网站的vitepress代码开源，你可以自由复制并在此部署。
 
 ---
 
